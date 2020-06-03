486a1cf43a0f00264be4ece4c74aed542f78413e
Yoni Schirris <yschirris@gmail.com>
2020-06-03 13:21:03 +0200

    add explanation at top of file

--- msidata/save_feature_vectors.py
+++ msidata/save_feature_vectors.py
@@ -1,3 +1,9 @@
+# Written by Yoni Schirris
+# Wed 3 June 2020
+# Script that transforms each tile given by a "regular" dataloader to a .pt feature vector with the same name
+# This makes it easy to adjust existing dataloaders to load these vectors instead of images
+# This script uses a pre-trained feature extractor, at the time of writing this is SimCLR
+
 from msidata.wip_patient_dataset_msi import PreProcessedMSIDataset as wip_dataset
 
 import torch
