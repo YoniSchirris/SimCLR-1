5b51501e648d2857326eeb302b55de4a0fb53d01
Yoni Schirris <yschirris@gmail.com>
2020-07-20 10:36:44 +0200

    fix byol unsupervised

--- feature_learning/main_unsupervised.py
+++ feature_learning/main_unsupervised.py
@@ -197,7 +197,7 @@ def main(_run, _log):
 
     if args.unsupervised_method=='byol':
         # Backbone is a reference to the network being used and updated. We should save the state of this network
-        model, optimizer, scheduler, backbone = load_model(args, train_loader, reload_model=args.reload_model, model_type=args.unsupervised_method)
+        model, optimizer, scheduler, backbone, _ = load_model(args, train_loader, reload_model=args.reload_model, model_type=args.unsupervised_method)
         
         # Criterion is defined within BYOL
         criterion = None
