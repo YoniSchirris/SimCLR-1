7709b18418fa34059c76d5d3a6ec44a5782e67a7
Yoni Schirris <yschirris@gmail.com>
2020-09-08 16:19:32 +0200

    update apex setup

--- setup_apex.sh
+++ setup_apex.sh
@@ -2,12 +2,20 @@
 eval "$(conda shell.bash hook)"
 
 # set CUDA_HOME (to PyTorch cuda version)
-export CUDA_HOME=/usr/local/cuda-10.1
-
 # make directories for apex
 mkdir -p ~/lib && cd ~/lib
 git clone https://github.com/NVIDIA/apex
 cd apex
+module purge
+module load 2019
+module load pre2019
+module load CUDA/10.1.243
+module load cuDNN/7.6.3-CUDA-10.1.243
+export LD_LIBRARY_PATH=/hpc/eb/Debian9/cuDNN/7.6.3-CUDA-10.1.243/lib64:$LD_LIBRARY_PATH
+module load Anaconda3
 
+
+CONDA_PREFIX=$(conda info --base)
+source $CONDA_PREFIX/etc/profile.d/conda.sh
 # install apex
-conda activate simclr && pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
+conda activate thesisp375 && pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
