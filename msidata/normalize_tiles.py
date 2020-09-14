c4ce2ecd7b229fb39b4106f84d21a5f3fa907f25
Yoni Schirris <yschirris@gmail.com>
2020-09-14 12:07:30 +0200

    make normalize_tiles script ready for use on lisa

--- msidata/normalize_tiles.py
+++ msidata/normalize_tiles.py
@@ -1,8 +1,8 @@
 import os
+import torch
 torch.set_num_threads(1)
 os.environ["OMP_NUM_THREADS"] = "1"
 import random
-import torch
 import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data.sampler import SubsetRandomSampler
@@ -14,6 +14,9 @@ from msidata.dataset_msi import PreProcessedMSIDataset as dataset_msi
 from msidata.save_feature_vectors import infer_and_save, aggregate_patient_vectors
 from msidata.dataset_msi_features_with_patients import PreProcessedMSIFeatureDataset
 from msidata.dataset_tcga_tiles import TiledTCGADataset as dataset_tcga
+
+from modules.transformations.colour_normalization import MyHETransform
+
 
 import pandas as pd
 import time
@@ -28,8 +31,8 @@ def main():
 
     # Set arguments
     workers=6
-    path_to_msi_data = '/project/yonis/brca_dx/tiled_data_large/'
-    root_dir_for_tcga_tiles = '/home/yonis/histogenomics-msc-2019/yoni-code/MsiPrediction/metadata/tcga/brca_dx/data_tcga_brca_dxwith_ddr_labels_and_splits.csv'
+    root_dir_for_tcga_tiles = '/project/yonis/tcga_brca_dx/tiled_data_large/'
+    path_to_msi_data = '/home/yonis/histogenomics-msc-2019/yoni-code/MsiPrediction/metadata/tcga/brca_dx/data_tcga_brca_dxwith_ddr_labels_and_splits.csv'
     kfold = 1
     ddr_label=None
     batch_size=128
@@ -68,15 +71,17 @@ def main():
             # Save images to disk
             print(f"[{datetime.datetime.now()} | Step {step} / {len(loader)}]")
             tiles, labels, patient_ids, img_names = data
-            for tile, label, patient_id, img_name in zip(tiles, labels, patient_ids, img_names):
+            for tile, patient_id, img_name in zip(tiles, patient_ids, img_names):
                  # Save each tile separately
                 tile = torchvision.transforms.functional.to_pil_image(tile)
                 new_img_name = img_name.replace('.jpg', '_norm_mac_kath.jpg')
-                new_absolute_tile_path = os.path.join(root_dir, new_img_name)
+                new_absolute_tile_path = os.path.join(root_dir_for_tcga_tiles, new_img_name)
                 #  assert(not os.path.isfile(new_absolute_tile_path)), f"{new_absolute_tile_path} already exists..."
-                if os.path.isfile(new_absolute_tile_path):
-                    print(f"{new_absolute_tile_path} already exists! Overwriting for now!")
+                #if os.path.isfile(new_absolute_tile_path):
+                #    print(f"{new_absolute_tile_path} already exists! Overwriting for now!")
+                #print(new_absolute_tile_path)
                 tile.save(new_absolute_tile_path)
+                assert(os.path.isfile(new_absolute_tile_path)), "Never saved?"
 
           
 
